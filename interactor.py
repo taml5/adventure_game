5414fb711ba4da690f5ad3d078064c51af8309ba
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-13 16:57:32 -0500

    Created interactor.py and main.py

---
+++ interactor.py
@@ -0,0 +1,45 @@
+"""The engine of the adventure game.
+
+TODO: write module docstring
+"""
+from entities.player import Player
+
+
+class Interactor:
+    """..."""
+    player: Player
+    # TODO: add controller
+    # TODO: add presenter
+
+    def __init__(self, player: Player) -> None:
+        self.player = player
+
+    def move_rooms(self, direction: str) -> None:
+        """Attempt to move the player into a neighbouring room."""
+        neighbouring_rooms = self.player.location.neighbours
+        if direction in neighbouring_rooms:
+            self.player.location = neighbouring_rooms[direction]
+            return None
+        else:
+            return None
+
+    def pickup_item(self, item_id: int) -> None:
+        """Pick an item up from the player's location"""
+        if self.player.has_item(item_id):
+            return None
+
+        location = self.player.location
+        if location.contains(item_id):
+            self.player.add_item(location.pop_item(item_id))
+            return None
+        else:
+            return None
+
+    def drop_item(self, item_id: int) -> None:
+        """..."""
+        if self.player.has_item(item_id):
+            item = self.player.inventory[item_id]
+            self.player.drop_item(item)
+            return None
+        else:
+            return None
---
+++ main.py
@@ -0,0 +1,7 @@
+"""The main class of the program.
+
+Initialises all the required classes, loads the needed data, and prepares the presenter."""
+
+
+if __name__ == "__main__":
+    ...
