9634aa3b1932b3c76d9c1928714dfadaa67b7b80
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-13 18:16:41 -0500

    Refactored player.add_item to work with containers, and added some docstrings

--- entities/player.py
+++ entities/player.py
@@ -1,25 +1,36 @@
-"""The player that is controlled and interacts with the world.
-
-TODO: write module docstring
+"""The player that is controlled by the user and interacts with the world.
 """
-from entities.item import Item
+from entities.item import Item, Container
 from entities.room import Room
 
 
 class Player:
     """
-    TODO: write docstring
+    Class representing the player.
+
+    inventory: the player's inventory. This is represented as a dictionary of the item id, and the item. inventory
+               cannot contain Containers.
+    location: the player's current location.
     """
     inventory: dict[int: Item]
     location: Room
 
     def __init__(self, room: Room):
         self.location = room
-        self.inventory = {}  # when player is initialised, inventory should be empty
+        self.inventory = {}
 
     def add_item(self, item: Item) -> None:
-        """Add an item into the player's inventory."""
-        self.inventory[item.id] = item
+        """Add an item into the player's inventory. If the item is a container, then recursively add its
+        items into the player's inventory.
+        """
+        if isinstance(item, Container):
+            # output something here about the container
+            for container_item in item.contents.values:
+                self.add_item(container_item)
+            return None
+        else:
+            self.inventory[item.id] = item
+            return None
 
     def drop_item(self, item: Item) -> None:
         """Remove an item from the player's inventory, and add it to the room.
@@ -31,5 +42,5 @@ class Player:
         self.location.contents[item.id] = Item
 
     def has_item(self, item_id: int) -> bool:
-        """..."""
+        """Returns if the player has the item."""
         return item_id in self.inventory
