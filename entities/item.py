febbd9c384580dce818f98a479092e57250d033e
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-16 18:20:56 -0500

    Wrote docstrings for item.py

--- entities/item.py
+++ entities/item.py
@@ -1,13 +1,19 @@
-"""TODO: write module docstring"""
+"""Defines the item entity and its subclasses."""
 
 
 class Item:
-    """TODO: write docstring"""
+    """An item in the game.
+
+    item_id: the id of the item which the engine uses to keep track of the item.
+    name: the name of the item.
+    description: a description of the item.
+    portable: if the player can pick the item up or not.
+    events: a collection of events associated with this item that can occur.
+    """
     item_id: int
     name: str
     description: str
-    _interactable: bool
-    _portable: bool
+    portable: bool
 
     def __init__(self, item_id: int):
         self.item_id = item_id
@@ -18,7 +24,10 @@ class Item:
 
 
 class Container(Item):
-    """TODO: write docstring"""
+    """An item that can contain other items.
+
+    contents: items that the Container contains, represented as a dictionary mapping from item_id to the Item.
+    """
     contents: dict[int: Item]
 
     def __init__(self, item_id: int):
