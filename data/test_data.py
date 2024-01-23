6cdbf3f0bfbcf5294ed2cfa9b555a1257f9f0219
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-23 14:34:53 -0500

    Formatted test_data.py to use the new entities

--- data/test_data.py
+++ data/test_data.py
@@ -1,57 +1,58 @@
 """This module contains some test items and rooms that can be used to test the game."""
 
-from system import Item, Room, Container
+from entities.room import Room
+from entities.item import Item, Container
 
 ################################################################
 # ITEMS
 ################################################################
 
 # test_room1
-test_key = Item(name='Rusty Key',
-                description='The key is old and rusted.',
-                keywords={'key', 'rusty key'},
-                portable=True,
-                interactable=True
-                )
-test_item1 = Item(name='Sword',
-                  description='A one-handed sword made of bronze, forged in a bygone era by an ancient '
-                              'civilisation. It\'s still sharp.',
-                  keywords={'sword'},
-                  portable=True,
-                  interactable=True
-                  )
-test_chest = Container(name='Old Chest',
-                       description='The chest is ancient: the wood is rotted and covered in moss. '
-                                   'Yet, the lock still holds firm.',
-                       locked=True,
-                       key=test_key,
-                       contents={test_item1},
-                       keywords={'chest', 'container'},
-                       portable=False,
-                       interactable=True
-                       )
+test_key = Item(item_id=hash('Key1'))
+test_key.set_attributes(name='Rusty Key',
+                        description='The key is old and rusted.',
+                        portable=True,
+                        interactable=True
+                        )
+test_item1 = Item(item_id=hash('Sword'))
+test_item1.set_attributes(name='Sword',
+                          description='A one-handed sword made of bronze, forged in a bygone era by an ancient '
+                                      'civilisation. It\'s still sharp.',
+                          portable=True,
+                          interactable=True
+                          )
+test_chest = Container(hash('chest1'))
+test_chest.set_attributes(name='Old Chest',
+                          description='The chest is ancient: the wood is rotted and covered in moss. '
+                                      'Yet, the lock still holds firm.',
+                          portable=False,
+                          interactable=True
+                          )
+test_chest.container_attributes(locked=True,
+                                key=test_key,
+                                contents={test_item1})
+
 # test_room2
-test_item2 = Item(name='Ruby Pendant',
-                  description='A pendant of gold with a large ruby inlaid in the middle. The gold is dull, '
-                              'but the ruby still shines brightly despite the dim light in the room.',
-                  keywords={'pendant', 'ruby pendant', 'gold pendant'},
-                  portable=True,
-                  interactable=True
-                  )
+test_item2 = Item(hash('pendant1'))
+test_item2.set_attributes(name='Ruby Pendant',
+                          description='A pendant of gold with a large ruby inlaid in the middle. The gold is dull, '
+                                      'but the ruby still shines brightly despite the dim light in the room.',
+                          portable=True,
+                          interactable=True
+                          )
 
 ################################################################
 # ROOMS
 ################################################################
 
-test_room1 = Room(name='Test Room',
-                  description='This is a test room. There is a rusty key and a chest.',
-                  contents={test_key, test_chest},
-                  locked=False
-                  )
-test_room2 = Room(name='Secret Room',
-                  description='This is another room connected to the main room. '
-                              'There is a pendant lying on the floor.',
-                  contents={test_item2},
-                  south=test_room1,
-                  locked=False
-                  )
+test_room1 = Room()
+test_room1.name = 'Test Room'
+test_room1.description = 'This is a test room. There is a rusty key and a chest.',
+test_room1.add_item(test_key)
+test_room1.add_item(test_chest)
+
+test_room2 = Room()
+test_room2.name = 'Secret Room'
+test_room2.description = 'This is another room connected to the main room. There is a pendant lying on the floor.'
+test_room2.add_item(test_item2)
+test_room2.add_neighbour(test_room1, 'south')
