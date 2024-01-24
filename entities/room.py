e104e787219aa233a082b3d27e5e00969087a81f
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-24 00:49:22 -0500

    Added support for interactions

--- entities/room.py
+++ entities/room.py
@@ -2,6 +2,7 @@
 
 This class is an entity that serves as the building blocks for the map of the game.
 """
+from entities.interaction import Interaction
 from entities.item import Item
 
 DIRECTIONS = {"north": "south", "east": "west", "south": "north", "west": "east",
@@ -16,12 +17,15 @@ class Room:
     visited: whether if the player has already visited this room or not.
     contents: a dictionary of item ids and the item that it contains.
     neighbours: a dictionary of directions and a connected room in that direction that the player can move to.
+    interactions: a collection of Interactions associated with the room that updates the game state after
+                  certain conditions are fulfilled.
     """
     name: str
     description: str
     visited: bool
     contents: dict[int: Item]
     neighbours: dict[str: 'Room']
+    interactions: set[Interaction]
 
     def __int__(self, name: str, description: str):
         self.name = name
@@ -29,6 +33,7 @@ class Room:
         self.visited = False
         self.contents = {}
         self.neighbours = {}
+        self.interactions = set()
 
     def contains(self, item_id: int) -> bool:
         """Returns whether an item is in the room."""
@@ -42,7 +47,7 @@ class Room:
         """Remove an item from the contents of the room and return it."""
         return self.contents.pop(item_id)
 
-    def add_neighbour(self, neighbour: 'Room', direction: str):
+    def add_neighbour(self, neighbour: 'Room', direction: str) -> None:
         """Add a neighbour to this room.
 
         Preconditions
@@ -52,3 +57,18 @@ class Room:
         """
         self.neighbours[direction] = neighbour
         neighbour.neighbours[DIRECTIONS[direction]] = self
+
+    def add_interaction(self, interaction: Interaction) -> None:
+        """Add an interaction that takes place in this room."""
+        self.interactions.add(interaction)
+
+    def execute_interaction(self, item: Item) -> tuple[bool, str | None]:
+        """Given an item, try to use the item in the room by finding a valid interaction. If found,
+        execute it and return a tuple containing True and its output message. Otherwise, return a tuple
+        containing False and None.
+        """
+        for interaction in self.interactions:
+            if item in interaction:
+                return True, interaction.execute_interaction()
+
+        return False, None
