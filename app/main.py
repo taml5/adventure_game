5f1719b9e266219e5ae44378fec25accdef7aa4e
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-24 16:25:42 -0500

    Updated the docstring and added a TODO

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
 """The main class of the program.
 
-Initialises all the required classes, loads the needed data, and prepares the presenter."""
+Defines the necessary API endpoints given by Flask, and calls the game factory to initialise the game engine. There
+are two defined endpoints:
+ - the default route '/' loads the webpage through rendering index.html.
+ - the route '/execute_command' defines a POST request where the webpage provides a command in JSON format.
+   The game engine processes the command and returns a string.
+"""
 
 from flask import Flask, render_template, request
 from markupsafe import escape
 
 app = Flask(import_name=__name__)
-
+# TODO: write and add GameFactory to initialise game
 
 @app.route('/')
 def index():
