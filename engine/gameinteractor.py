5d5b1109c89f600ee006522472f5ebb78b9aaa50
tamlong5 <dexter.tam@mail.utoronto.ca>
2024-01-24 18:08:16 -0500

    Implemented "Help", "Look", "Go", and error behaviours, and modified others to return list[str]

--- engine/gameinteractor.py
+++ engine/gameinteractor.py
@@ -5,75 +5,74 @@ to the Presenter to be shown to the player.
 """
 from entities.player import Player
 from entities.item import Item, Container
-from engine.interactor_input_data import InteractorData
-
-VALID_COMMANDS = {"move", "go", "get", "drop"}
 
 
 class GameInteractor:
-    """..."""
+    """TODO: write docstring
+
+    TODO: change all command methods to return a list of strings instead
+    """
     player: Player
-    # TODO: add presenter
 
     def __init__(self, player: Player) -> None:
         self.player = player
 
-    def parse_data(self, input_data: InteractorData):
-        """Given some InteractorData, parse the commands and execute the necessary logic."""
-        command = input_data.command
-        if command not in VALID_COMMANDS:
-            return None
-        elif command == "move" or command == "go":
-            direction = input_data.direction
-            self.move_rooms(direction)
-        elif command == "get":
-            item = input_data.item_ids[0]
-            self.pickup_item(item)
-        elif command == "drop":
-            item = input_data.item_ids[0]
-            self.drop_item(item)
-
-    def move_rooms(self, direction: str) -> None:
+    def move_rooms(self, direction: str) -> list[str]:
         """Attempt to move the player into a neighbouring room."""
         neighbouring_rooms = self.player.location.neighbours
         if direction in neighbouring_rooms:
             self.player.location = neighbouring_rooms[direction]
-            return None
+            if not self.player.location.visited:
+                return self.describe_room()
+            else:
+                return [self.player.location.name]
         else:
-            return None
+            return ["You can't move that way!"]
 
-    def pickup_item(self, item_id: int) -> None:
+    def pickup_item(self, item_id: int) -> list[str]:
         """Pick an item up from the player's location and place it in the player's inventory.
         """
-        item = self.find_item(item_id)
+        item, in_inventory = self.find_item(item_id)
         if item is None:  # item not found
-            return None
-        elif item[1]:  # item is already in the inventory
-            return None
+            return ["I can't find that item."]
+        elif in_inventory:  # item is already in the inventory
+            return ["You already have that."]
         elif not item[0].portable:  # cannot pick up that item
-            return None
+            return ["You can't pick that up."]
         else:
             item = self.player.location.pop_item(item_id)
             if isinstance(item, Container):  # different behaviour if item is a Container
+                output = []
                 for subitem in item.contents:
-                    self.player.add_item(subitem)
-                return None
+                    output.extend(self.pickup_item(subitem.item_id))
+                return output
             else:
                 self.player.add_item(item)
-                return None
+                return [f"Picked up {item.name}."]
 
-    def drop_item(self, item_id: int) -> None:
+    def drop_item(self, item_id: int) -> list[str]:
         """Attempt to drop an item from the player's inventory. If the player does not have it, do nothing and
         inform the player.
         """
         if self.player.has_item(item_id):
             item = self.player.inventory[item_id]
             self.player.drop_item(item)
-            return None
+            return [f"Dropped {item.name}."]
         else:
-            return None
+            return [f"You don't have that."]
 
-    def find_item(self, item_id: int) -> tuple[Item, bool] | None:
+    def get_help(self) -> list[str]:
+        """Return the help command.
+
+        TODO: write a better help response
+        """
+        return ["Good luck!"]
+
+    def handle_invalid_event(self) -> list[str]:
+        """Return a string informing the player that they have entered an invalid command."""
+        return ["I don't know you're trying to say."]
+
+    def find_item(self, item_id: int) -> tuple[Item, bool] | tuple[None, None]:
         """Find the given item in the player's vicinity (location or inventory). If found, return the item and
         if it is in the player's inventory as a tuple. Otherwise, return None."""
         if self.player.has_item(item_id):
@@ -81,4 +80,12 @@ class GameInteractor:
         elif self.player.location.contains(item_id):
             return (self.player.location.contents[item_id], False)
         else:
-            return None
+            return None, None
+
+    def describe_room(self) -> list[str]:
+        """Return the description of the room."""
+        if self.player.location.visited:
+            return [self.player.location.description]
+        else:
+            self.player.location.visited = True
+            return [self.player.location.name, self.player.location.description]
